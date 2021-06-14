db5484fedbd58a74b3292646f1789015116f9476
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-06-14 19:25:11 -0300

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { in: 5..50 }, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: { min: 8 }
 end
